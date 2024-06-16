6363c25ce85c9491239ae55df2a1f0380bf7c578
Simone Cesano <simonecesano@libero.it>
2024-06-16 09:42:19 +0200

    First commit

---
+++ textures/__init__.py
@@ -0,0 +1,12 @@
+from .circles import *
+from .lines   import *
+from .paths   import *
+
+
+class Texture:
+    def lines(self, *args):
+        return Lines(*args)
+    def circles(self, *args):
+        return Circles(*args)
+    def paths(self, *args):
+        return Paths(*args)
