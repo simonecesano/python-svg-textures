5196de5c2cd736a3747444f74da2f192b6ca54fa
Simone Cesano <simonecesano@libero.it>
2024-12-14 15:56:07 +0100

    Cleanup

--- textures/base.py
+++ textures/base.py
@@ -4,46 +4,47 @@ import string
 class BaseTexture:
     def random_id(self):
         return ''.join(random.choices(string.ascii_lowercase, k=5))
+
     def __str__(self):
         return self.svg()
         
-    def set_background(self, color):
-        self.background = color
+    def background(self, color):
+        self._background = color
         return self
 
-    def set_size(self, size):
-        self.size = size
+    def size(self, size):
+        self._size = size
         return self
-    def set_stroke(self, color):
-        self.stroke = color
-        return self
-
-    def set_stroke_width(self, width):
-        self.stroke_width = width
+    def stroke(self, color):
+        self._stroke = color
         return self
 
-    def set_id(self, value=None):
+    def stroke_width(self, width):
+        self._stroke_width = width
+        return self
+
+    def id(self, value=None):
         if value is None:
             return self.id
-        self.id = value
+        self._id = value
         return self
 
     def heavier(self, factor=2):
-        self.stroke_width *= factor
+        self._stroke_width *= factor
         return self
 
     def lighter(self, factor=2):
-        self.stroke_width /= factor
+        self._stroke_width /= factor
         return self
 
     def thinner(self, factor=2):
-        self.size *= factor
+        self._size *= factor
         return self
 
     def thicker(self, factor=2):
-        self.size /= factor
+        self._size /= factor
         return self
 
     def url(self):
-        return f"url(#{self.id})"
+        return f"url(#{self._id})"
     
