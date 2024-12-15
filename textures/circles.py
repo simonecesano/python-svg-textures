5cec2c79193d4e5233e4cfaeea1dfcf3cdccabcb
Simone Cesano <simonecesano@libero.it>
2024-12-15 15:04:56 +0100

    Cleanup

--- textures/circles.py
+++ textures/circles.py
@@ -7,11 +7,11 @@ class Circles(BaseTexture):
     def __init__(self):
         self.size = 20
         self.background = ''
-        self.radius = 2
-        self.complement = False
-        self.fill = '#343434'
-        self.stroke = '#343434'
-        self.stroke_width = 0
+        self._radius = 2
+        self._complement = False
+        self._fill = '#343434'
+        self._stroke = '#343434'
+        self._stroke_width = 0
         self.id = self.random_id()
 
     def svg(self):
@@ -20,59 +20,31 @@ class Circles(BaseTexture):
 
         if self.background:
             svg.append(f'<rect width="{self.size}" height="{self.size}" fill="{self.background}" />')
-        svg.append(f'<circle cx="{self.size / 2}" cy="{self.size / 2}" r="{self.radius}" fill="{self.fill}" stroke="{self.stroke}" stroke-width="{self.stroke_width}" />')
+        svg.append(f'<circle cx="{self.size / 2}" cy="{self.size / 2}" r="{self._radius}" fill="{self._fill}" stroke="{self._stroke}" stroke-width="{self._stroke_width}" />')
         if self.complement:
             for corner in [(0, 0), (0, self.size), (self.size, 0), (self.size, self.size)]:
-                svg.append(f'<circle cx="{corner[0]}" cy="{corner[1]}" r="{self.radius}" fill="{self.fill}" stroke="{self.stroke}" stroke-width="{self.stroke_width}" />')
+                svg.append(f'<circle cx="{corner[0]}" cy="{corner[1]}" r="{self._radius}" fill="{self._fill}" stroke="{self._stroke}" stroke-width="{self._stroke_width}" />')
 
         svg.append('</pattern>')
         return '\n'.join(svg)
 
 
     def complement(self, value=True):
-        self.complement = value
+        self._complement = value
         return self
 
     def radius(self, radius):
-        self.radius = radius
+        self._radius = radius
         return self
 
     def fill(self, color):
-        self.fill = color
+        self._fill = color
         return self
 
     def heavier(self, factor=2):
-        self.radius *= factor
+        self._radius *= factor
         return self
 
     def lighter(self, factor=2):
-        self.radius /= factor
+        self._radius /= factor
         return self
-
-    # ----------------------------------------------------------------------
-
-    # def thinner(self, factor=2):
-    #     self.size *= factor
-    #     return self
-
-    # def thicker(self, factor=2):
-    #     self.size /= factor
-    #     return self
-    
-    # def stroke(self, color):
-    #     self.stroke = color
-    #     return self
-
-    # def stroke_width(self, width):
-    #     self.stroke_width = width
-    #     return self
-
-    # def id(self, value=None):
-    #     if value is None:
-    #         return self.id
-    #     self.id = value
-    #     return self
-
-    # def url(self):
-    #     return f"url(#{self.id})"
-
